171ee5422e52b473a1f24007d8222bf663ef4c49
hanmangniu <h15867105305@163.com>
2020-08-20 15:53:23 +0800

    8.20

---
+++ utils/ball.js
@@ -0,0 +1,35 @@
+function Ball(x, y, radius, color) {
+  //圆形中心x坐标
+  this.x = x || 0;
+  //圆形中心y坐标
+  this.y = y || 0;
+  //圆形半径
+  this.radius = radius || 12;
+  //颜色
+  this.color = color || "pink";
+
+  this.scaleX = 1;
+  this.scaleY = 1;
+}
+Ball.prototype = {
+  stroke: function (cxt) {
+    cxt.save();
+    cxt.scale(this.scaleX, this.scaleY);
+    cxt.strokeStyle = this.color;
+    cxt.beginPath();
+    cxt.arc(this.x, this.y, this.radius, 0, (360 * Math.PI) / 180, false);
+    cxt.closePath();
+    cxt.stroke();
+    cxt.restore();
+  },
+  fill: function (cxt) {
+    cxt.save();
+    cxt.scale(this.scaleX, this.scaleY);
+    cxt.fillStyle = this.color;
+    cxt.beginPath();
+    cxt.arc(this.x, this.y, this.radius, 0, (360 * Math.PI) / 180, false);
+    cxt.closePath();
+    cxt.fill();
+    cxt.restore();
+  },
+};
